d16ad0d013403b100558dbadaf4974acb0731f0f
Leonardo Carvalho <leo.freitascarvalho@gmail.com>
2026-07-03 10:57:35 -0300

    remove: excluindo pasta duplicada

--- core/admin.py
+++ core/admin.py
@@ -7,14 +7,11 @@ from loja.models import Documento, ItemDocumento, MovimentacaoEstoque
 @admin.register(Documento)
 class DocumentoAdmin(admin.ModelAdmin):
     """Registra o modelo Documento dentro do bloco de Movimentações."""
-    pass
 
 @admin.register(ItemDocumento)
 class ItemDocumentoAdmin(admin.ModelAdmin):
     """Registra o modelo ItemDocumento dentro do bloco de Movimentações."""
-    pass
 
 @admin.register(MovimentacaoEstoque)
 class MovimentacaoEstoqueAdmin(admin.ModelAdmin):
     """Registra o modelo MovimentacaoEstoque dentro do bloco de Movimentações."""
-    pass
--- loja/tests.py
+++ loja/tests.py
@@ -1,3 +1,4 @@
+""" Arquivo de testes para a aplicação 'loja' do projeto Django. """
 from django.test import TestCase
 
 # Create your tests here.
